375b5b95285becaba2d75019fc7f5d2362fabf61
pepe <franck.calestroupat@gmail.com>
2024-10-17 23:00:34 +0200

    Creation d'un panneauPersonalScroe qui affichera dans le panneau NORTH le score d'un joueur

--- src/vues/panneaux/PanScores_north.java
+++ src/vues/panneaux/PanScores_north.java
@@ -2,16 +2,26 @@ package vues.panneaux;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JPanel;
 
 public class PanScores_north extends JPanel {
 	
 	public PanScores_north(int x, int y) {
+		
 		//transformation de y double en entier avec calcul de pourcentage
-		int height = (int) Math.round(y * 0.2);
+		int height = (int) Math.round(y * 0.2); //hauteur du panneau de scores NORTH
 		this.setPreferredSize(new Dimension(x, height));
 		
+		//hauteur du panneauPersonnel affiché
+		int heightFils = (int) Math.round(height * 0.85);
+		this.setLayout(new FlowLayout(0, 10, (int) Math.round((height - heightFils) / 2)));	//FlowLayout​(int align, int hgap, int vgap)
+		
 		this.setBackground(Color.BLUE);
+		
+		PanPersonalScore psc = new PanPersonalScore(heightFils);
+		
+		this.add(psc);
 	}
 }
