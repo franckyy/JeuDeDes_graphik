2808489079ca8ea3b4f40721c715f1283c17e070
pepe <franck.calestroupat@gmail.com>
2024-11-07 07:50:22 +0100

    Travail sur le panneau du centre pour avoir un panneau en haut pour les messages et un panneau en bas pour afficher les dés

--- src/vues/panneaux/PanDes_center.java
+++ src/vues/panneaux/PanDes_center.java
@@ -1,10 +1,12 @@
 package vues.panneaux;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,12 +27,30 @@ public class PanDes_center extends JPanel {
 	JButton button;
 	
 	Dimension dim = null;
+	
+	JPanel panMessages = null;
+	JPanel panDes = null;
 
 	//*********CONSTRUCTEUR*********
 	public PanDes_center(Control ctrl_, Dimension dim_) {
 		this.ctrl = ctrl_;
 		this.dim = dim_;
 		this.setPreferredSize(dim);
+		
+		panMessages = new JPanel();
+		panDes = new JPanel();
+		
+		panMessages.setBackground(Color.red);
+		panDes.setBackground(Color.pink);
+		
+		this.setAlignmentX(CENTER_ALIGNMENT);
+		
+		this.add(panMessages);
+		this.add(panDes);
+		
+		
 	}
+
+
 	//*********METHODES*********
 }
