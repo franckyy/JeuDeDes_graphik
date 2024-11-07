f9276284a0b2bfc2f073b529e982dfc9143b044a
pepe <franck.calestroupat@gmail.com>
2024-11-07 23:09:26 +0100

    Création d'un package "south" Déplacement de la classe PanCommands_south dans ce nouveau package

--- src/vues/PanneauPrincipal.java
+++ src/vues/PanneauPrincipal.java
@@ -8,9 +8,9 @@ import javax.swing.JPanel;
 import ctrl.Control;
 import modeles.Joueur;
 import vues.panneaux.PanActualScore_east;
-import vues.panneaux.PanCommands_south;
 import vues.panneaux.center.Pan_center;
 import vues.panneaux.north.PanScores_north;
+import vues.panneaux.south.PanCommands_south;
 
 public class PanneauPrincipal extends JPanel {
 
--- src/vues/panneaux/south/PanCommands_south.java
+++ src/vues/panneaux/south/PanCommands_south.java
@@ -1,4 +1,4 @@
-package vues.panneaux;
+package vues.panneaux.south;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
