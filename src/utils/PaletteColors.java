503d72ca3244baa69b6d863296f118b933bb89b2
franckyy <franck.calestroupat@protonmail.com>
2024-11-13 15:49:46 +0100

    Création d'une classe PaletteColors pour la gestion des couleurs

--- src/utils/PaletteColors.java
+++ src/utils/PaletteColors.java
@@ -1,5 +1,14 @@
 package utils;
 
-public class PaletteColors {
+import java.awt.Color;
 
+public class PaletteColors {
+    public static final Color VIOLET = new Color(128, 0, 128); // violet neutre
+    public static final Color VIOLET_DARK = new Color(75, 0, 130); // violet plus foncé
+    public static final Color VIOLET_LIGHT = new Color(238, 130, 238); // violet clair
+    public static final Color MYSTIC_PURPLE = new Color(153, 50, 204); // Violet mystique
+    
+    public static final Color BACKGROUND = new Color(245, 245, 245); // couleur de fond neutre
+
+    public static final Color BUTTON_COLOR = new Color(200, 100, 255); // couleur pour les boutons
 }
