ceae8e005b974e0a4be799d82984dd3538cf8f12
pepe <franck.calestroupat@gmail.com>
2024-11-14 00:52:29 +0100

    Suppression du dégradé dans le panneau des dés. Ajout d'un cadre autour des dés avec gestion de la couleur du cadre.

--- src/vues/panneaux/center/PanneauDes_Center.java
+++ src/vues/panneaux/center/PanneauDes_Center.java
@@ -1,15 +1,13 @@
 package vues.panneaux.center;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RadialGradientPaint;
 import java.awt.RenderingHints;
-
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-
 import ctrl.Control;
 import utils.PaletteColors;
 
@@ -17,130 +15,101 @@ public class PanneauDes_Center extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
-	final Color coulBackPanel = PaletteColors.BACKGROUND_DES;
-	
-    Control ctrl = null;
+    private final Color coulBackPanel = PaletteColors.BACKGROUND_DES;
+    private Color frameColor = PaletteColors.SECOND_VERT_4; // Couleur par défaut du cadre
 
-    // Tableau pour stocker les valeurs des dés
-    private int[] valeursDes = {1, 2, 3, 4, 5};  // Valeurs initiales
-    private Color[] couleursDes = {Color.white, Color.white, Color.white, Color.white, Color.white};  // Couleurs initiales des dés
+    private Control ctrl = null;
+    private int[] valeursDes = {1, 2, 3, 4, 5};
+    private Color[] couleursDes = {Color.white, Color.white, Color.white, Color.white, Color.white};
 
-    //*********CONSTRUCTEUR*********
     public PanneauDes_Center(Control ctrl_) {
-    	System.out.println("PanneauDes_Center(Control ctrl_)");
         this.ctrl = ctrl_;
         ctrl.setPanDes(this);
         this.setBackground(coulBackPanel);
     }
 
-    //*********METHODES*********
+    // Méthode pour définir la couleur du cadre
+    public void setFrameColor(Color color) {
+        this.frameColor = color;
+        repaint();
+    }
 
     @Override
     public Dimension getPreferredSize() {
-        // Définir la taille du panneau bas à deux tiers de la hauteur du PanneauCentre
         Dimension dim = super.getPreferredSize();
-        dim.height = getParent().getHeight() * 2 / 3;  // Deux tiers de la hauteur du PanneauCentre
-        dim.width = 600;  // Largeur fixe pour les dés
+        dim.height = getParent().getHeight() * 2 / 3;
+        dim.width = 600;
         return dim;
     }
 
-    // Méthode pour mettre à jour les valeurs des dés
     public void setValeursDes(int[] valeurs) {
-    	System.out.println("PanneauDes_Center - void setValeursDes(int[] valeurs)");
         if (valeurs != null && valeurs.length == 5) {
             this.valeursDes = valeurs;
-            repaint();  // Redessiner le panneau avec les nouvelles valeurs
+            repaint();
         }
     }
 
-    // Méthode pour griser les dés spécifiés
     public void griserDes(int[] griserDes) {
-    	System.out.println("PanneauDes_Center - void griserDes(int[] griserDes)");
         for (int i = 0; i < griserDes.length; i++) {
             if (griserDes[i] == -1) {
-                couleursDes[i] = Color.gray;  // Changer la couleur du dé en gris
-            }
-            else {
-                couleursDes[i] = Color.white;  // Changer la couleur du dé en gris
+                couleursDes[i] = Color.gray;
+            } else {
+                couleursDes[i] = Color.white;
             }
         }
-
-        // Utiliser invokeLater pour garantir le redessin sur le thread d'interface graphique
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                repaint();
-            }
-        });
+        SwingUtilities.invokeLater(this::repaint);
     }
-    
+
     @Override
-    public void paint(Graphics g) {
-    	System.out.println("PanneauDes_Center - void paint(Graphics g)");
-        super.paint(g);
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D) g;
-        // Améliorer la qualité du rendu graphique
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Créer un dégradé radial qui part du centre du panneau vers les bords
-        int centerX = getWidth() / 2;
-        int centerY = getHeight() / 2;
-        
-        // Utiliser des rayons différents pour l'axe horizontal et l'axe vertical (créant un effet ovale)
-        int radiusX = getWidth() / 5;   // Rayon horizontal (plus large)
-        int radiusY = getHeight() / 7;  // Rayon vertical (plus étroit)
-        
-        Color color1 = coulBackPanel;  // Couleur du centre
-        Color color2 = PaletteColors.BACKGROUND_SECOND;  // Couleur des bords
-        // Créer un dégradé radial oval qui part du centre du panneau vers les bords
-        RadialGradientPaint gradient = new RadialGradientPaint(centerX, centerY, Math.max(radiusX, 50), new float[]{0.5f, 1.0f}, new Color[]{color1, color2});
-        
-        
-        // Appliquer le dégradé de fond
-        g2d.setPaint(gradient);
-        g2d.fillRect(0, 0, getWidth(), getHeight());
-        
-        // Déterminer les dimensions des dés en pourcentage de la taille du panneau
+        // Dessiner le cadre autour du panneau
+        int frameThickness = 20;
+        g2d.setColor(frameColor);
+        g2d.setStroke(new BasicStroke(frameThickness));
+
+        int inset = frameThickness / 2;
+        g2d.drawRect(inset, inset, getWidth() - frameThickness, getHeight() - frameThickness);
+
+        // Appeler la méthode pour dessiner les dés
+        paintDice(g2d);
+    }
+
+    private void paintDice(Graphics2D g2d) {
         int panelWidth = getWidth();
         int panelHeight = getHeight();
 
-        // Taille des dés : 10% de la largeur et 20% de la hauteur du panneau
-        int tailleDe = Math.min(panelWidth, panelHeight) / 3;  // Taille des dés basée sur la taille du panneau
-        int espacement = tailleDe / 2;  // Espacement entre les dés (moitié de la taille d'un dé)
+        int tailleDe = Math.min(panelWidth, panelHeight) / 3;
+        int espacement = tailleDe / 2;
 
-        // Coordonnées de départ pour le premier dé
-        int xStart = (panelWidth - (tailleDe * 5 + espacement * 4)) / 2;  // Centrer horizontalement
-        int yStart = panelHeight / 2 - tailleDe / 2; // Centrer verticalement
+        int xStart = (panelWidth - (tailleDe * 5 + espacement * 4)) / 2;
+        int yStart = panelHeight / 2 - tailleDe / 2;
 
-        // Dessiner les 5 dés
         for (int i = 0; i < 5; i++) {
-            // Calculer la position de chaque dé
             int x = xStart + i * (tailleDe + espacement);
-
-            // Dessiner le dé (rectangle avec coins arrondis)
             g2d.setColor(couleursDes[i]);
-            g2d.fillRoundRect(x, yStart, tailleDe, tailleDe, 15, 15); // coins arrondis
-
-            // Dessiner le contour du dé
-            g2d.setColor(Color.black);
-            g2d.drawRoundRect(x, yStart, tailleDe, tailleDe, 15, 15);
+            g2d.fillRoundRect(x, yStart, tailleDe, tailleDe, 15, 15);
 
             // Dessiner les points sur le dé en fonction de la valeur
             dessinerPoints(g2d, valeursDes[i], x, yStart, tailleDe);
         }
     }
 
-    // Méthode pour dessiner les points sur un dé
     private void dessinerPoints(Graphics2D g2d, int valeur, int x, int y, int taille) {
-        int pointSize = taille / 8;  // Taille des points (10% de la taille du dé)
+        int pointSize = taille / 8;  // Taille des points
 
+        g2d.setColor(Color.black); // Couleur des points
         switch (valeur) {
             case 1:
                 g2d.fillOval(x + taille / 2 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize);
                 break;
             case 2:
                 g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
-                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / pointSize, pointSize, pointSize);
+                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize);
                 break;
             case 3:
                 g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
@@ -156,17 +125,17 @@ public class PanneauDes_Center extends JPanel {
             case 5:
                 g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
                 g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + taille / 2 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize);
                 g2d.fillOval(x + taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize);
                 g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize);
-                g2d.fillOval(x + taille / 2 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize);
                 break;
             case 6:
-                g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize); // Haut gauche
-                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize); // Haut droite
-                g2d.fillOval(x + taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize); // Bas gauche
-                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize); // Bas droite
-                g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize); // Milieu gauche
-                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize); // Milieu droite
+                g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + taille / 4 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + 3 * taille / 4 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + taille / 4 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize);
+                g2d.fillOval(x + 3 * taille / 4 - pointSize / 2, y + taille / 2 - pointSize / 2, pointSize, pointSize);
                 break;
         }
     }
